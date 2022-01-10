232846e3f6a9e46518134d9e1074d12686a23085
Joey Marino <joeymarino94@gmail.com>
2022-01-10 11:22:55 -0600

    back to basics

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import './portfolio.scss'
-import Cards from '../cards/Card.jsx'
+
 
 
 export default function portfolio() {
     return (
         <div className='portfolio' id="portfolio">
-            <Cards />
+            
         </div>
     )
 }
--- src/Components/topbar/Topbar.jsx
+++ src/Components/topbar/Topbar.jsx
@@ -5,18 +5,7 @@ import { MailOutline } from '@material-ui/icons'
 export default function topbar() {
     return (
         <div className='topbar' id="topbar">
-            <div className='wrapper'>
-                <div className="left">
-                    <a href='#intro' className='lefttext'>Joey Marino</a>
-                </div>
-                <div className='center'>
-                   
-                </div>
-                <div className="right">
-                    <MailOutline className='mailicon'/>
-                    <a href='#contact' className='righttext'>Contact me</a>
-                </div>
-            </div>
+          
         </div>
     )
 }
