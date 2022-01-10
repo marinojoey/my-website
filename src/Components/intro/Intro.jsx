8f517ac8d349c1f5fe08b3165e84a510bcda405c
Joey Marino <joeymarino94@gmail.com>
2022-01-10 17:30:40 -0600

    intro page started

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,15 +1,27 @@
 import './intro.scss'
+import { ArrowDownward } from '@material-ui/icons';
 
 export default function intro() {
     return (
         <div className='intro' id="intro">
             <div className='left'> 
-                <div className="imgcontainer">
-                    <img src="assets/belaying.jpg" alt="me rock climbing"></img>
+                <div className="top">
+                    <img src="assets/dronepic3.jpg" alt="Sav Duke and I" class="dronepic"></img>
+                </div>
+                <div className='bottom'>
+                    <img src="assets/belaysq.jpeg" alt="me climbing"></img>
                 </div>
             </div>
             <div className="right">
-                
+                <div className="wrapper">
+                    <h2>Hi there, my name is:</h2>
+                    <h1>Joey Marino.</h1>
+                    <h3>I am a person.</h3>
+                </div>
+                <a href="#portfolio" >
+                    <span className='linktext'>Take a look at what I have made </span>
+                    <ArrowDownward className='arrow'/>
+                </a>
             </div>
         </div>
     )
