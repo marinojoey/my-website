354d8e439af4506af344dd8f45e19c5e5461369a
Joey Marino <joeymarino94@gmail.com>
2022-01-10 11:42:05 -0600

    progress

--- src/Components/topbar/Topbar.jsx
+++ src/Components/topbar/Topbar.jsx
@@ -1,11 +1,18 @@
 import './topbar.scss'
-import { MailOutline } from '@material-ui/icons'
+// import { MailOutline } from '@material-ui/icons'
 
 
 export default function topbar() {
     return (
         <div className='topbar' id="topbar">
-          
+          <div className='wrapper'>
+              <div className="left">
+                <a href='#intro' className='title'>Joey Marino</a>
+              </div>
+              <div className="right">
+
+              </div>
+          </div>
         </div>
     )
 }
