133a4554a4c77d451f66c89e0732ee70cfc27b23
Joey Marino <joeymarino94@gmail.com>
2022-01-12 17:35:53 -0600

    good changes to contact form

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -4,22 +4,32 @@ import { MailOutline } from '@material-ui/icons'
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            <div className='contact-container'>
-                <div className="email">
-                    <MailOutline className='mailicon'/>
-                    <a href="mailto:marinojoey@pm.me" className='emailaddress'>Marinojoey@pm.me</a>
-                </div>
-                <div className="linked-in">
-                    <img src="/assets/linked-in.png" alt="linked-in logo" className='linked-in-logo'></img>
-                    <a href="https://www.linkedin.com/in/joeymarino1994/" className='linked-in-address'>Linked-in</a>
-                </div>
-                <div className="github">
-                    <img src="/assets/githublogo.png" alt="github logo" className='github-logo'></img>
-                    <a href="https://github.com/marinojoey" className='github-link'>Github</a>
-                </div>
-                <div className="ENS">
-                    <img src="/assets/ethlogo.png" alt="ethereum diamond" className='ethlogo'></img>
-                    <a href="https://etherscan.io/address/0xa23d143e9ee88f7982c7f1136459962c09ba7dc7" className='etherscan'>Marinojoey.Eth</a>
+            <div className="wrapper">
+                <div className='grid-container'>
+                    <div className="left-one">
+                        <MailOutline className='mail-icon'/>
+                    </div>
+                    <div className="right-one">
+                        <a href="mailto:marinojoey@pm.me" className='email-address'>Marinojoey@pm.me</a>
+                    </div>
+                    <div className="left-two">
+                        <img src="/assets/linked-in.png" alt="linked-in logo" className='linked-in-logo'></img>
+                    </div>
+                    <div className="right-two">
+                        <a href="https://www.linkedin.com/in/joeymarino1994/" className='linked-in-link'>linkedin.com/in/joeymarino1994/</a>
+                    </div>
+                    <div className="left-three">
+                        <img src="/assets/githublogo.png" alt="github logo" className='github-logo'></img>
+                    </div>
+                    <div className="right-three">
+                        <a href="https://github.com/marinojoey" className='github-link'>github.com/marinojoey</a>
+                    </div>
+                    <div className="left-four">
+                        <img src="/assets/ethlogo.png" alt="ethereum diamond" className='eth-logo'></img>
+                    </div>
+                    <div className="right-four">
+                        <a href="https://etherscan.io/address/0xa23d143e9ee88f7982c7f1136459962c09ba7dc7" className='eth-link'>Marinojoey.Eth</a>
+                    </div>
                 </div>
             </div>
         </div>
