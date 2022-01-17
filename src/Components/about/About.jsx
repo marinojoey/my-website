4d55c9d3dad15dbe1e0178df01b8d0461c6eaa59
Joey Marino <joeymarino94@gmail.com>
2022-01-17 15:39:47 -0600

    wrote about me page

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -2,8 +2,22 @@ import './about.scss'
 
 export default function About() {
     return (
-        <div className='works' id="works">
-            ABOUTTTT
+        <div className='about' id="about">
+            <div className="top">
+                About:
+            </div>
+            <div className="left">
+                <div className="content-wrapper-left">
+                    Growing up I never imagined that I would have a desk job. I thought a life spent in front of a computer screen was incompatible with who I was. This ideology shaped my decisions through college and beyond- choosing degrees and paths that kept me "in the real world". That is until one day I had the same sort of realization many have: I get <strong className='bolded'>so much</strong> value, convenience, entertainment, productivity and general use out of technology that it very much is "the real world". The idea started to grow that maybe I should start learning it... and maybe even build something. <br></br> <br></br>
+                    It was a good thought at least.       In reality I kept on going down the same path I had been: working "in the real world". 
+                </div>
+            </div>
+            <div className="right">
+                <div className="content-wrapper-right">
+                    The turning point for me was when I started learning about Bitcoin. Specifically the humanitarian and philosophical components of it. Sitting in front of me was a highly technical protocol running a global network of computers. The realization that if I wanted to work in the "real world", one of the most meaningful ways that I could do that was via software hit me square in the head. <br></br> <br></br>
+                    As a consequence, I quit my job and devoted myself to learning software development. Six months later, you've happened to stumble onto my humble website. I would love to hear from you if you would like to work together, have any feedback and especially if you want to share your favorite joke.
+                </div>
+            </div>
         </div>
     )
 }
